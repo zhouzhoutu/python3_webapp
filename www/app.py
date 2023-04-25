a62ea67935d8b0803e3907128adf8b78dcb4ff7c
2357 <congeez@gmx.com>
2023-04-25 10:51:16 +0800

    add crete_pool

--- www/app.py
+++ www/app.py
@@ -1,23 +1,44 @@
-import logging; logging.basicConfig(level=logging.INFO)
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 import asyncio, os, json, time
 from datetime import datetime
 
 from aiohttp import web
 
+
 def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
+    return web.Response(body=b"<h1>Awesome</h1>", content_type="text/html")
+
 
 @asyncio.coroutine
 def init(loop):
     app = web.Application(loop=loop)
-    app.router.add_route('GET', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-    logging.info('server started at http://127.0.0.1:9000...')
+    app.router.add_route("GET", "/", index)
+    srv = yield from loop.create_server(app.make_handler(), "127.0.0.1", 9000)
+    logging.info("server started at http://127.0.0.1:9000...")
     return srv
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
 
 
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    logging.info("creating datebase connection pool...")
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host=kw.get("host", "localhost"),
+        port=kw.get("port", 3306),
+        user=kw["user"],
+        password=kw["password"],
+        db=kw["db"],
+        charset=kw.get("charset", "utf8"),
+        autocommit=kw.get("autocommit", True),
+        maxsize=kw.get("maxsize", 10),
+        minsize=kw.get("minsize", 1),
+        loop=loop,
+    )
